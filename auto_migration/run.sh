4b7e721a34f3088d0ed39bfc77a186600630b404
Heyuan Liu <heyuan.liu@intel.com>
2023-01-13 09:42:49 +0800

    add commands to run.sh

--- auto_migration/run.sh
+++ auto_migration/run.sh
@@ -1,1 +1,15 @@
 ./mvnw process-test-classes -Dfiles=/home/heyuan/openrewrite_demo/spring-petclinic/target/classes
+
+./mvnw org.openrewrite.maven:rewrite-maven-plugin:4.22.1:init -Ddependencies=org.openrewrite.recipe:rewrite-testing-frameworks:1.20.1 -DactiveRecipes=org.openrewrite.java.testing.junit5.JUnit5BestPractices
+
+./mvnw rewrite:configure -Ddependencies=org.openrewrite.recipe:rewrite-spring:4.19.2 -DactiveRecipes=org.openrewrite.java.spring.boot2.SpringBoot1To2Migration
+
+./mvnw rewrite:run
+
+./mvnw rewrite:configure -Ddependencies=org.openrewrite.recipe:rewrite-migrate-java:1.4.2 -DactiveRecipes=org.openrewrite.java.migrate.Java8toJava11
+
+./mvnw rewrite:run
+
+sdk use java 11.0.17-albba
+
+./mvnw verify
